3d838238fcc9737c01d6edb7b24d5c8247daf0b3
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2024-07-22 13:55:03 +0300

    adding ssl connections for endpoints

--- certbot/generate_ssl_certificates.sh
+++ certbot/generate_ssl_certificates.sh
@@ -1,13 +1,5 @@
-certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=infosystem_ssl --key-type rsa --agree-tos
-certbot certonly --standalone --email $DOMAIN_EMAIL -d $API_DOMAIN_URL --cert-name=api_infosystem_ssl --key-type rsa --agree-tos
-
-rm -rf /etc/nginx/cert.pem
-rm -rf /etc/nginx/key.pem
-rm -rf /etc/nginx/api_cert.pem
-rm -rf /etc/nginx/api_key.pem
+certbot certonly --webroot --email $DOMAIN_EMAIL -d $DOMAIN -d $API_DOMAIN -w /var/www/certbot --keep-until-expiring --cert-name=infosystem_ssl --key-type rsa --rsa-key-size 2048 --agree-tos
 
 cp /etc/letsencrypt/live/infosystem_ssl/fullchain.pem /etc/nginx/cert.pem
 cp /etc/letsencrypt/live/infosystem_ssl/privkey.pem /etc/nginx/key.pem
-
-cp /etc/letsencrypt/live/api_infosystem_ssl/fullchain.pem /etc/nginx/api_cert.pem
-cp /etc/letsencrypt/live/api_infosystem_ssl/privkey.pem /etc/nginx/api_key.pem
+cp /etc/letsencrypt/live/infosystem_ssl/chain.pem /etc/nginx/trusted_cert.pem

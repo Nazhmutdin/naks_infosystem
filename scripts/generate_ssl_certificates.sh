2fe9ca50fc57fc6d9a60b758ec0863c6a99fe245
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2024-11-15 09:23:00 +0300

    frontend: frontend submodule added; nginx: updates for frontend routing; backend: new updates

--- scripts/generate_ssl_certificates.sh
+++ scripts/generate_ssl_certificates.sh
@@ -2,8 +2,8 @@ DOMAIN=$(python-dotenv get DOMAIN)
 DOMAIN_EMAIL=$(python-dotenv get DOMAIN_EMAIL)
 API_DOMAIN=$(python-dotenv get API_DOMAIN)
 
-sudo certbot certonly --webroot -w /var/www/certbot --email $DOMAIN_EMAIL -d $DOMAIN -d $API_DOMAIN --keep-until-expiring --cert-name=infosystem_ssl --key-type rsa --rsa-key-size 2048 --agree-tos
+sudo certbot certonly --webroot -w /var/www/certbot --email $DOMAIN_EMAIL -d $DOMAIN -d $API_DOMAIN --cert-name=infosystem_ssl --key-type rsa --rsa-key-size 2048 --agree-tos
 
-cp /etc/letsencrypt/live/infosystem_ssl/fullchain.pem ../nginx/ssl/cert.pem
-cp /etc/letsencrypt/live/infosystem_ssl/privkey.pem ../nginx/ssl/key.pem
-cp /etc/letsencrypt/live/infosystem_ssl/chain.pem ../nginx/ssl/trusted_cert.pem
+cp /etc/letsencrypt/live/infosystem_ssl/fullchain.pem ./nginx/ssl/cert.pem
+cp /etc/letsencrypt/live/infosystem_ssl/privkey.pem ./nginx/ssl/key.pem
+cp /etc/letsencrypt/live/infosystem_ssl/chain.pem ./nginx/ssl/trusted_cert.pem

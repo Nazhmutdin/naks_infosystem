8beb919774adcddb6b950351b215ca0aba44f611
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2024-11-20 14:29:35 +0300

    files service service submodule added

--- scripts/generate_ssl_certificates.sh
+++ scripts/generate_ssl_certificates.sh
@@ -1,8 +1,9 @@
 DOMAIN=$(python-dotenv get DOMAIN)
 DOMAIN_EMAIL=$(python-dotenv get DOMAIN_EMAIL)
 API_DOMAIN=$(python-dotenv get API_DOMAIN)
+FILES_DOMAIN=$(python-dotenv get FILES_DOMAIN)
 
-sudo certbot certonly --webroot -w /var/www/certbot --email $DOMAIN_EMAIL -d $DOMAIN -d $API_DOMAIN --cert-name=infosystem_ssl --key-type rsa --rsa-key-size 2048 --agree-tos
+sudo certbot certonly --webroot -w /var/www/certbot --email $DOMAIN_EMAIL -d $DOMAIN -d $API_DOMAIN -d $FILES_DOMAIN --cert-name=infosystem_ssl --key-type rsa --rsa-key-size 2048 --agree-tos
 
 cp /etc/letsencrypt/live/infosystem_ssl/fullchain.pem ./nginx/ssl/cert.pem
 cp /etc/letsencrypt/live/infosystem_ssl/privkey.pem ./nginx/ssl/key.pem
